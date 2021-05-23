0df8eae5616b90f44895e504849da7823dfe9f84
huanglei <huanglei@quanjingshuju.com>
2021-05-23 09:11:33 +0800

    test

--- headless.py
+++ headless.py
@@ -3,8 +3,14 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 #chrome_options.add_argument("--disable-extensions")
 #chrome_options.add_argument("--disable-gpu")
-chrome_options.binary_location ="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
+
 driver.get('http://www.baidu.com')
+driver.find_element_by_id("kw").send_keys("selenium webdriver")
+driver.find_element_by_id("su").click()
+driver.save_screenshot('screen.png')
+
+driver.close()
+
 print(driver.page_source)
